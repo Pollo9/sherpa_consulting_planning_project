9108c537bb98991f162d64a7b44bd3aca0402b43
LucaSappey <lucas.sappey@yahoo.fr>
2020-03-03 13:38:55 +0100

    messagerie fonctionne
    Signed-off-by: LucaSappey <lucas.sappey@yahoo.fr>

--- sherpa_consulting_planning_project/settings.py
+++ sherpa_consulting_planning_project/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'planning.context_processors.notif_total'
             ],
         },
     },
